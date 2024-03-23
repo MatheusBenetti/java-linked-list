02b224bd448a0955a059ecac2f02c431086c53ee
MatheusBenetti <mbbenetti1@gmail.com>
2024-03-23 14:55:58 -0300

    added new methods to LinkedList

--- src/LinkedLists/LinkedList.java
+++ src/LinkedLists/LinkedList.java
@@ -150,6 +150,32 @@ public class LinkedList {
         }
     }
 
+    public Node findMiddleNode() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean hasNode() {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
